fc81fd82ab6f5e336c048c23b1f42761527771b8
reem <reemalsofayan@gmail.com>
2020-08-13 21:45:58 +0300

    ergwgegg

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+DEBUG = True 
 #database_path = 'postgresql://postgres:root@localhost:5432/castingagency'
 # Connect to the database
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/castingagency'
+DATABASE_URL = 'postgresql://postgres:root@localhost:5432/castingagency'
 
