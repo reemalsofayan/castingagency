b6fddb48e3db9338fc77da39b18dacf27562083d
reem <reemalsofayan@gmail.com>
2020-08-13 21:50:23 +0300

    drgerge

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
 import os
-
-SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True 
-#database_path = 'postgresql://postgres:root@localhost:5432/castingagency'
-# Connect to the database
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-DATABASE_URL = 'postgresql://postgres:root@localhost:5432/castingagency'
 
+class Config(object):
+    """Add configuration variables."""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
