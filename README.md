2274048021763f602e7fe495d69da63b79de52db
reem <reemalsofayan@gmail.com>
2020-08-16 20:08:31 +0300

    formatt and readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-################ casting-agency ##########################33
+################ casting-agency ##########################
 
 The same proposed  udacity capstone project.
 
@@ -16,12 +16,53 @@ application live at[ https://castingagency-deploy.herokuapp.com/]
 - [Flask-Migrate](https://flask-migrate.readthedocs.io/en/latest/#)
 - [alembic](https://alembic.sqlalchemy.org/en/latest/)
 
+## Dependencies
 
 install the requiremnets file using 
 pip install -r requirements.txt
 
 
-how to run the app=> go the the project directory and run
+## Motivation
+
+this is the final project of FSND in order to graduate
+
+
+
+## API Authentication: 
+
+you can log in to the API using the link:
+
+https://fsnd-castingagency.au.auth0.com/authorize?
+ audience=Casting&
+ response_type=token&
+ client_id=7b4nVmc85DgE0ZSkw2zytQHHxjA23OMd&
+ redirect_uri=https://castingagency-deploy.herokuapp.com/
+
+using the following users :
+
+-EXECUTIVE_PRODUCER
+
+USERNAME: executive_producer@casting.com
+PASSWORD: executive_producer123
+
+
+-CASTING_DIRECTOR 
+
+USERNAME: casting_director@casting.com
+PASSWORD: casting_director123
+
+
+-CASTING_ASSISTANT
+
+USERNAME: casting_assistant@casting.com
+PASSWORD: casting_assistant123
+
+
+
+
+## How to run the app
+
+go the the project directory and run
 
 export FLASK_APP=app
 export FLASK_DEBUG=True
@@ -31,6 +72,171 @@ flask run
 
 ## Unit tests
 
-how to run the app=> go the the project directory and run
+go the the project directory and run
 
 python test.py
+
+
+
+##endpoints
+
+ the endpoints are tested using postman tool
+
+### GET http://127.0.0.1:5000/movies
+
+Response:
+
+{
+  "movies": [
+    {
+      "id": 132,
+      "release_date": 2017,
+      "title": "The Accountant"
+    }
+  ],
+  "success": true
+}
+
+//---------------------------------------------------
+### POST http://127.0.0.1:5000/movies
+
+DATA:
+
+ {
+         "title":"The Accountant",
+         "release_date":2017
+        
+
+      }
+
+Response:
+
+{
+  "movie": {
+    "id": 132,
+    "release_date": 2017,
+    "title": "The Accountant"
+  },
+  "success": true
+}
+
+//------------------------------------------------------
+
+### PATCH http://127.0.0.1:5000/movies/132
+
+DATA:
+ {
+         "title":"The Accountant",
+         "release_date":2016
+        
+
+            }
+
+Response:
+
+{
+  "movie": {
+    "id": 132,
+    "release_date": 2016,
+    "title": "The Accountant"
+  },
+  "success": true
+}
+
+//---------------------------------
+
+
+
+### DELETE http://127.0.0.1:5000/movies/132
+
+
+Response:
+
+
+{
+  "delete": 132,
+  "success": true
+}
+
+//-------------------------------
+
+### GET http://127.0.0.1:5000/actors
+
+
+
+Response:
+
+{
+  "actors": [
+    {
+      "age": 48,
+      "gender": "male",
+      "id": 67,
+      "name": "richard Armitage"
+    }
+  ],
+  "success": true
+}
+```
+//-------------------------------------------------
+### POST http://127.0.0.1:5000/actors
+
+
+Data:
+
+{
+  "name": "richard Armitage",
+  "age":48,
+  "gender": "male"
+
+}
+
+Response:
+
+{
+  "actor": {
+    "age": 48,
+    "gender": "male",
+    "id": 67,
+    "name": "richard Armitage"
+  },
+  "success": true
+}
+
+//---------------------------------
+### PATCH http://127.0.0.1:5000/actors/67
+
+Edit data on a actor in the db.
+
+Data:
+
+{
+  "name": "james Armitage",
+  "age":48,
+  "gender": "male"
+
+}
+
+
+Response:
+
+{
+  "actor": {
+    "age": 48,
+    "gender": "male",
+    "id": 67,
+    "name": "james Armitage"
+  },
+  "success": true
+}
+//-----------------------------------------
+
+### DELETE http://127.0.0.1:5000/actors/67
+
+
+Response:
+
+{
+  "delete": 67,
+  "success": true
+}
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+from models import *
 import sys
 import json
 import dateutil.parser
 import babel
 from sqlalchemy import func
-from flask import Flask, render_template, request, Response, flash, redirect, url_for, abort
+from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
@@ -14,20 +15,19 @@ from auth import AuthError, requires_auth
 import os
 
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from models import *
 
 # a welcoming endpoint to the application
 @app.route('/')
 def index():
 
     return jsonify({
-        'message': 'Welcome to Casting Agency , you may start to explore the endpoints',
+        'message': 'Welcome to Casting Agency project'
+
         'success': True
     })
 
@@ -37,18 +37,16 @@ def index():
 def add_movie(payload):
     # get the body request
     body = request.get_json()
-    title=body.get('title',None)
-    release_date=body.get('release_date',None)
-    
-    
-    if  title is None or release_date is None :
+    title = body.get('title', None)
+    release_date = body.get('release_date', None)
+
+    if title is None or release_date is None:
         abort(422)
     try:
 
-        new_movie = Movie(title=title,release_date=release_date)
+        new_movie = Movie(title=title, release_date=release_date)
         new_movie.insert()
-            
-        
+
         return jsonify({
             'movie': new_movie.format(),
             'success': True
@@ -61,14 +59,14 @@ def add_movie(payload):
 @app.route('/movies')
 @requires_auth('get:movies')
 def get_movies(payload):
-    
+
     # try:
-        movies = Movie.query.all()
-        movies = [movie.format() for movie in movies]
-        return jsonify({
-            'success': True,
-            'movies': movies
-        })
+    movies = Movie.query.all()
+    movies = [movie.format() for movie in movies]
+    return jsonify({
+        'success': True,
+        'movies': movies
+    })
     # except:
     #     abort(422)
 # this endpint will alow the user to modify an existing movie
@@ -76,8 +74,8 @@ def get_movies(payload):
 @requires_auth('patch:movies')
 def modify_movie(payload, movie_id):
     body = request.get_json()
-    title = body.get('title',None)
-    release_date = body.get('release_date',None)
+    title = body.get('title', None)
+    release_date = body.get('release_date', None)
 
     try:
         # check if the movie exist in the database
@@ -88,22 +86,21 @@ def modify_movie(payload, movie_id):
         if release_date is not None:
             movie.release_date = release_date
         else:
-            abort(400)      
+            abort(400)
 
         if title is not None:
-            movie.title = title 
+            movie.title = title
         else:
-            abort(400)  
+            abort(400)
 
-
-        movie.update()  
+        movie.update()
 
         return jsonify({
             'success': True,
             'movie': movie.format()
         })
 
-    except:   
+    except BaseException:
         abort(422)
 
 
@@ -113,7 +110,7 @@ def modify_movie(payload, movie_id):
 def delete_movie(payload, movie_id):
     movie = Movie.query.filter_by(id=movie_id).one_or_none()
 
-    if  movie is None:
+    if movie is None:
         abort(404)
 
     try:
@@ -121,41 +118,38 @@ def delete_movie(payload, movie_id):
         return jsonify({
             'success': True,
             'delete': movie_id
-            })
-    except:
+        })
+    except BaseException:
         abort(422)
 
 # this endpint will alow the user to add an actor to database
 @app.route('/actors', methods=['POST'])
 @requires_auth('post:actors')
 def add_actor(payload):
-    
+
     body = request.get_json()
-    name = body.get('name',None)
-    age = body.get('age',None)
-    gender = body.get('gender',None)
-    
-    
+    name = body.get('name', None)
+    age = body.get('age', None)
+    gender = body.get('gender', None)
+
     if name is None:
         abort(422)
 
     if age is None:
-        abort(422) 
+        abort(422)
 
     if gender is None:
-        abort(422)       
+        abort(422)
 
-    
     try:
-        new_actor = Actor(name=name,age=age,gender=gender)
+        new_actor = Actor(name=name, age=age, gender=gender)
         new_actor.insert()
-            
-        
+
         return jsonify({
             'actor': new_actor.format(),
             'success': True
         })
-    except:
+    except BaseException:
         abort(422)
 
 
@@ -163,17 +157,16 @@ def add_actor(payload):
 @app.route('/actors')
 @requires_auth('get:actors')
 def get_actors(payload):
-    
+
     try:
         actors = Actor.query.all()
         actors = [actor.format() for actor in actors]
-        
 
         return jsonify({
-            'actors':actors,
+            'actors': actors,
             'success': True
         })
-    except:
+    except BaseException:
         abort(422)
 
 # this endpint will alow the user to modify an existing actor
@@ -182,9 +175,9 @@ def get_actors(payload):
 def modify_actor(payload, actor_id):
     body = request.get_json()
 
-    name = body.get('name',None)
-    age = body.get('age',None)
-    gender = body.get('gender',None)
+    name = body.get('name', None)
+    age = body.get('age', None)
+    gender = body.get('gender', None)
 
     try:
         # check if actor exist in database
@@ -193,31 +186,28 @@ def modify_actor(payload, actor_id):
             abort(404)
 
         if name is not None:
-            actor.name  = name  
+            actor.name = name
         else:
             abort(400)
-        
 
         if age is not None:
             actor.age = age
         else:
-            abort(400)    
+            abort(400)
 
         if gender is not None:
-            actor.gender  = gender   
+            actor.gender = gender
         else:
-            abort(400)        
+            abort(400)
 
-
-
-        actor.update()  
+        actor.update()
 
         return jsonify({
             'success': True,
             'actor': actor.format()
         })
 
-    except:   
+    except BaseException:
         abort(422)
 
 
@@ -227,7 +217,7 @@ def modify_actor(payload, actor_id):
 def delete_actor(payload, actor_id):
     actor = Actor.query.filter_by(id=actor_id).one_or_none()
 
-    if  actor is None:
+    if actor is None:
         abort(404)
 
     try:
@@ -235,14 +225,12 @@ def delete_actor(payload, actor_id):
         return jsonify({
             'success': True,
             'delete': actor_id
-            })
+        })
 
-    except:
+    except BaseException:
         abort(422)
 
-
     # return app
-
 
 
 @app.errorhandler(422)
@@ -254,9 +242,6 @@ def unprocessable(error):
     }), 422
 
 
-
-
-
 @app.errorhandler(AuthError)
 def auth_error(error):
     return jsonify({
@@ -264,4 +249,3 @@ def auth_error(error):
         "error": error.status_code,
         "message": error.error
     }), error.status_code
-
